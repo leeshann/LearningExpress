39447d62bd844be1f7d4a3f78df526133882952b
Shannon Lee <lee.shannon1014@gmail.com>
2025-02-23 13:37:49 -0700

    adding query search functionality

--- app.js
+++ app.js
@@ -16,21 +16,52 @@ app.get('/', (req, res) => {
 app.get('/api/users', (req, res) => {
 
         const limitedUserInfo = fakeData.map((user) => {
-            const {first_name, last_name, email } = user
-            return {first_name, last_name, email }
+            const {id, first_name, last_name, email } = user
+            return {id, first_name, last_name, email }
         })
         
         res.json(limitedUserInfo)
 })
 
+//sets up parameters, anything that comes after the ":" is not a hardcoded value
 app.get('/api/users/:userID', (req, res) => {
 
-    //req.params.[paramater name] will always be returned as a STRING
+    //req.params.[paramater name] will always be returned as a STRING so be sure to handle accordingly
     const specificUser = fakeData.find((user) => {
         return user.id === Number(req.params.userID)
     })
 
+    if (!specificUser) {
+        res.status(404).send("<h1>User does not exist</h1>")
+    }
+
         res.json(specificUser)
+})
+
+// example url --> http://localhost:3000/api/users/lookup/query?gender=Female
+app.get('/api/users/lookup/query', (req, res) => {
+    const { gender, limit } = req.query
+    console.log(req.query)
+
+    let usersOfThisGender = [...fakeData]
+    // console.log(usersOfThisGender)
+
+    if (gender) {
+        usersOfThisGender = usersOfThisGender.filter((user) => {
+            return user.gender === gender
+        })
+    }
+
+    if (limit) {
+        usersOfThisGender = usersOfThisGender.slice(0, Number(limit))
+    } 
+
+    if (usersOfThisGender.length === 0) {
+        // res.status(200).send('<h1>No users matched the filters you searched for</h1>')
+        return res.status(200).json({success: true, data: []})
+    }
+
+    res.json(usersOfThisGender)
 })
 
 app.all('*', (req, res) => {
@@ -39,7 +70,7 @@ app.all('*', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log("server listening on port 3000")
+    console.log("server listening on port 3000...")
 })
 
 // app.get(path to resource, callback function (req, res) )
